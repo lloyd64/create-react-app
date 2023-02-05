6b10f33d737ead66c169b4dacc2f6706ba73446d
lloyd64 <122186067+lloyd64@users.noreply.github.com>
2023-02-05 18:00:54 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code> Hello </code> World
         </p>
         <a
           className="App-link"
@@ -18,7 +18,7 @@ function App() {
           Learn React
         </a>
       </header>
-<h1> Hello World </h1>
+
     </div>
   );
 }
