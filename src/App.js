5b8b7eb37914b5763ace058fb51f3d2f596716a1
lloyd64 <122186067+lloyd64@users.noreply.github.com>
2023-02-05 18:02:23 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code> Hello </code> World
+          <code> Hello </code> World
         </p>
         <a
           className="App-link"
